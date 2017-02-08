5ce0f0f9ac516bb595f3a03e0bad13f7537ea632
BlackFox <huangy_tao@126.com>
2017-02-08 23:28:27 +0800

    利用MFGT在GuideActivity中添加跳转到登录注册界面的逻辑

--- superwechat/src/cn/ucai/superwechat/ui/GuideActivity.java
+++ superwechat/src/cn/ucai/superwechat/ui/GuideActivity.java
@@ -1,15 +1,39 @@
 package cn.ucai.superwechat.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.utils.MFGT;
 
 public class GuideActivity extends BaseActivity {
+
+    @BindView(R.id.btn_login)
+    Button btnLogin;
+    @BindView(R.id.btn_register)
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_login, R.id.btn_register})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_login:
+                MFGT.gotoLogin(this);
+                break;
+            case R.id.btn_register:
+                MFGT.gotoRegister(this);
+                break;
+        }
     }
 }
