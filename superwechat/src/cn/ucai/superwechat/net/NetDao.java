b7f64ec1bc16f4f3c18423fa36ec666c498f1dda
blackfox <huangy_tao@126.com>
2017-02-10 10:20:06 +0800

    NetDao添加登录注册自己服务器的方法

---
+++ superwechat/src/cn/ucai/superwechat/net/NetDao.java
@@ -0,0 +1,46 @@
+package cn.ucai.superwechat.net;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+import cn.ucai.superwechat.I;
+import cn.ucai.superwechat.utils.MD5;
+import cn.ucai.superwechat.utils.OkHttpUtils;
+
+/**
+ * Created by Administrator on 2017/2/9 0009.
+ */
+
+public class NetDao {
+    public static void register(Context context, String username, String nick, String password
+                               , OkHttpUtils.OnCompleteListener <String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_REGISTER)
+                .addParam(I.User.USER_NAME,username)
+                .addParam(I.User.NICK,nick)
+                .addParam(I.User.PASSWORD, MD5.getMessageDigest(password))
+                .targetClass(String.class)
+                .post()
+                .execute(listener);
+    }
+
+    public static void unRegister(Context context, String username, OkHttpUtils.OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UNREGISTER)
+                .addParam(I.User.USER_NAME,username)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    public static void Login(Context context, String username,
+                             String password, OkHttpUtils.OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_LOGIN)
+                .addParam(I.User.USER_NAME,username)
+                .addParam(I.User.PASSWORD,password)
+                .targetClass(String.class)
+                .execute(listener);
+
+
+    }
+}
