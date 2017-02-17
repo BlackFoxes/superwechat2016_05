387867a96461e9f414923f2813a1589b2216e99c
blackfox <huangy_tao@126.com>
2017-02-17 20:03:36 +0800

    创建ProfileFragment类并完成其布局文件的编写

---
+++ superwechat/src/cn/ucai/superwechat/ui/ProfileFragment.java
@@ -0,0 +1,169 @@
+/**
+ * Copyright (C) 2016 Hyphenate Inc. All rights reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.ucai.superwechat.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.easemob.redpacketui.utils.RedPacketUtil;
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMOptions;
+import com.hyphenate.easeui.utils.EaseUserUtils;
+import com.hyphenate.easeui.widget.EaseSwitchButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.ucai.superwechat.Constant;
+import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.SuperWechatModel;
+import cn.ucai.superwechat.utils.EaseCommonUtils;
+
+/**
+ * settings screen
+ *
+ *
+ */
+@SuppressWarnings({"FieldCanBeLocal"})
+public class ProfileFragment extends Fragment {
+
+    @BindView(R.id.iv_profile_avatar)
+    ImageView ivProfileAvatar;
+    @BindView(R.id.tv_profile_nickname)
+    TextView tvProfileNickname;
+    @BindView(R.id.tv_profile_username)
+    TextView tvProfileUsername;
+    /**
+     * new message notification
+     */
+    private RelativeLayout rl_switch_notification;
+    /**
+     * sound
+     */
+    private RelativeLayout rl_switch_sound;
+    /**
+     * vibration
+     */
+    private RelativeLayout rl_switch_vibrate;
+    /**
+     * speaker
+     */
+    private RelativeLayout rl_switch_speaker;
+
+
+    /**
+     * line between sound and vibration
+     */
+    private TextView textview1, textview2;
+
+    private LinearLayout blacklistContainer;
+
+    private LinearLayout userProfileContainer;
+
+    /**
+     * logout
+     */
+    private Button logoutBtn;
+
+    private RelativeLayout rl_switch_chatroom_leave;
+
+    private RelativeLayout rl_switch_delete_msg_when_exit_group;
+    private RelativeLayout rl_switch_auto_accept_group_invitation;
+    private RelativeLayout rl_switch_adaptive_video_encode;
+    private RelativeLayout rl_custom_appkey;
+    private RelativeLayout rl_custom_server;
+    RelativeLayout rl_push_settings;
+    private LinearLayout ll_call_option;
+
+    /**
+     * Diagnose
+     */
+    private LinearLayout llDiagnose;
+    /**
+     * display name for APNs
+     */
+    private LinearLayout pushNick;
+
+    private EaseSwitchButton notifySwitch;
+    private EaseSwitchButton soundSwitch;
+    private EaseSwitchButton vibrateSwitch;
+    private EaseSwitchButton speakerSwitch;
+    private EaseSwitchButton ownerLeaveSwitch;
+    private EaseSwitchButton switch_delete_msg_when_exit_group;
+    private EaseSwitchButton switch_auto_accept_group_invitation;
+    private EaseSwitchButton switch_adaptive_video_encode;
+    private EaseSwitchButton customServerSwitch;
+    private EaseSwitchButton customAppkeySwitch;
+    private SuperWechatModel settingsModel;
+    private EMOptions chatOptions;
+    private EditText edit_custom_appkey;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null && savedInstanceState.getBoolean("isConflict", false))
+            return;
+        initData();
+
+    }
+
+    private void initData() {
+        String username = EMClient.getInstance().getCurrentUser();
+        tvProfileUsername.setText(username);
+        EaseUserUtils.setAppUserNick(username,tvProfileNickname);
+        EaseUserUtils.setAppUserAvatar(getContext(),username,ivProfileAvatar);
+
+
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (((MainActivity) getActivity()).isConflict) {
+            outState.putBoolean("isConflict", true);
+        } else if (((MainActivity) getActivity()).getCurrentAccountRemoved()) {
+            outState.putBoolean(Constant.ACCOUNT_REMOVED, true);
+        }
+    }
+
+    @OnClick({R.id.layout_profile_view, R.id.tv_profile_money, R.id.tv_profile_settings})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.layout_profile_view:
+                break;
+            case R.id.tv_profile_money:
+                RedPacketUtil.startChangeActivity(getActivity());
+                break;
+            case R.id.tv_profile_settings:
+                break;
+        }
+    }
+}
