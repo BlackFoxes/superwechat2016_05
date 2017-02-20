f6c9979c65ff29b79bfb0db20bb6b9bb192966af
blackfox <huangy_tao@126.com>
2017-02-20 09:45:40 +0800

    UserProfileActivity中添加更新自己服务昵称后的逻辑处理

--- superwechat/src/cn/ucai/superwechat/ui/UserProfileActivity.java
+++ superwechat/src/cn/ucai/superwechat/ui/UserProfileActivity.java
@@ -32,10 +32,13 @@ import java.io.ByteArrayOutputStream;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.SuperWechatHelper;
 import cn.ucai.superwechat.domain.Result;
 import cn.ucai.superwechat.net.NetDao;
+import cn.ucai.superwechat.utils.CommonUtils;
+import cn.ucai.superwechat.utils.L;
 import cn.ucai.superwechat.utils.MFGT;
 import cn.ucai.superwechat.utils.OkHttpUtils;
 import cn.ucai.superwechat.utils.ResultUtils;
@@ -44,6 +47,7 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
 
     private static final int REQUESTCODE_PICK = 1;
     private static final int REQUESTCODE_CUTTING = 2;
+    private static final String TAG = UserProfileActivity.class.getSimpleName();
     @BindView(R.id.img_back)
     ImageView imgBack;
     @BindView(R.id.text_title)
@@ -169,35 +173,82 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
 
     private void updateRemoteNick(final String nickName) {
         dialog = ProgressDialog.show(this, getString(R.string.dl_update_nick), getString(R.string.dl_waiting));
-        new Thread(new Runnable() {
+        NetDao.updateUserNick(this, EMClient.getInstance().getCurrentUser(), nickName,
+                new OkHttpUtils.OnCompleteListener<String>() {
+                    @Override
+                    public void onSuccess(String s) {
+                        dialog.dismiss();
 
-            @Override
-            public void run() {
-                boolean updatenick = SuperWechatHelper.getInstance().getUserProfileManager().updateCurrentUserNickName(nickName);
-                if (UserProfileActivity.this.isFinishing()) {
-                    return;
-                }
-                if (!updatenick) {
-                    runOnUiThread(new Runnable() {
-                        public void run() {
-                            Toast.makeText(UserProfileActivity.this, getString(R.string.toast_updatenick_fail), Toast.LENGTH_SHORT)
-                                    .show();
-                            dialog.dismiss();
+                        if (s != null) {
+                            Result result = ResultUtils.getResultFromJson(s, User.class);
+                            if (result != null) {
+                                if (result.isRetMsg()) {
+                                    User user = (User) result.getRetData();
+                                    if (user != null) {
+                                        L.e(TAG,"user"+user);
+                                            CommonUtils.showShortToast(R.string.toast_updatenick_success);
+
+
+                                    }
+
+                                } else {
+                                    if (result.getRetCode() == I.MSG_USER_SAME_NICK) {
+                                        CommonUtils.showShortToast("昵称未修改");
+                                    } else {
+
+                                        CommonUtils.showShortToast(R.string.toast_updatenick_fail);
+                                    }
+                                }
+                            } else {
+                                CommonUtils.showShortToast(R.string.toast_updatenick_fail);
+
+                            }
+
+
+                        } else {
+                            CommonUtils.showShortToast(R.string.toast_updatenick_fail);
+
                         }
-                    });
-                } else {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            dialog.dismiss();
-                            Toast.makeText(UserProfileActivity.this, getString(R.string.toast_updatenick_success), Toast.LENGTH_SHORT)
-                                    .show();
-                            tvUserinfoNick.setText(nickName);
-                        }
-                    });
-                }
-            }
-        }).start();
+
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        L.e(TAG,"error="+error);
+                        dialog.dismiss();
+                        CommonUtils.showShortToast(R.string.toast_updatenick_fail);
+
+                    }
+                });
+//        new Thread(new Runnable() {
+//
+//            @Override
+//            public void run() {
+//                boolean updatenick = SuperWechatHelper.getInstance().getUserProfileManager().updateCurrentUserNickName(nickName);
+//                if (UserProfileActivity.this.isFinishing()) {
+//                    return;
+//                }
+//                if (!updatenick) {
+//                    runOnUiThread(new Runnable() {
+//                        public void run() {
+//                            Toast.makeText(UserProfileActivity.this, getString(R.string.toast_updatenick_fail), Toast.LENGTH_SHORT)
+//                                    .show();
+//                            dialog.dismiss();
+//                        }
+//                    });
+//                } else {
+//                    runOnUiThread(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            dialog.dismiss();
+//                            Toast.makeText(UserProfileActivity.this, getString(R.string.toast_updatenick_success), Toast.LENGTH_SHORT)
+//                                    .show();
+//                            tvUserinfoNick.setText(nickName);
+//                        }
+//                    });
+//                }
+//            }
+//        }).start();
     }
 
     @Override
@@ -292,7 +343,6 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
                 break;
             case R.id.layout_userinfo_avatar:
                 uploadHeadPhoto();
-
                 break;
             case R.id.layout_userinfo_nick:
                 final EditText editText = new EditText(this);
